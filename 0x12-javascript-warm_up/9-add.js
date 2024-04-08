5124d88ea9de93089d076bf606106f55b3724b7b
Faustin-king <f.nduwumwamiwabami@gmail.com>
2024-04-08 13:13:23 -0700

    Add 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,23 +1,8 @@
 #!/usr/bin/node
 
-// Define the add function
 function add (a, b) {
-  return a + b;
+  const c = a + b;
+  console.log(c);
 }
 
-// check if both arguments are provided
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log('Missing arguments');
-} else {
-  // Parse the arguments as intergers
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[3]);
-
-  // Check if both arguments are valid integers
-  if (!isNaN(num1) && !isNaN(num2)) {
-    // Call the add function and print the result
-    console.log('The addition is:', add(num1, num2));
-  } else {
-    console.log('Invalid arguments');
-  }
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
