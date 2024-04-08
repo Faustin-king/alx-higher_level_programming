7c614653d734c696412e9b7f12634468449c95f2
Faustin-king <f.nduwumwamiwabami@gmail.com>
2024-04-08 13:51:41 -0700

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 
-// Check if at least two arguments are passed to the script
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else {
-  console.log('Please provide two arguments');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
