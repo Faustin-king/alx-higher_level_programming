9f95f8fb4b1d062f8782875c0e77ec9e361afa56
Faustin-King <f.nduwumwamiwabami@gmail.com>
2024-09-26 07:22:09 -0700

    Add 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+# Import Rectangle class from module 9-rectangle
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size   # Import Rectangle class from module 9-rectangle
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
